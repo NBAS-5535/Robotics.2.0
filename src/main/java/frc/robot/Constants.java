e7e3cc97189e3a2f87fcf4d87975d65ccbbfadbe
NBAS-5535 <uras@yahoo.com>
2024-11-28 18:36:43 -0600

    small updates
    added xbox controller button mapping; drive kinematics 1.0

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -66,6 +66,10 @@ public final class Constants {
 
   public static double storeSpeed;
   
+  public static class DriveTrainConstants{
+    public static final double kTrackWidth = 27.; //inches
+  }
+
   public static class PIDConstants {
     public static final double kpSpeed = 1.0;
     public static final double kiSpeed = 0.;
@@ -77,3 +81,51 @@ public final class Constants {
   }
   
 }
+
+/*
+      // Default Values 
+      private static final double DEFAULT_THUMBSTICK_DEADZONE = 0.1;  // Jiggle room for the thumbsticks
+      private static final double DEFAULT_TRIGGER_DEADZONE    = 0.01; // Jiggle room for the triggers
+      private static final double DEFAULT_TRIGGER_SENSITIVITY = 0.6;  // If the trigger is beyond this limit, say it has been pressed
+      
+      // Button Mappings
+      private static final int    A_BUTTON_ID                 = 1;
+      private static final int    B_BUTTON_ID                 = 2;
+      private static final int    X_BUTTON_ID                 = 3;
+      private static final int    Y_BUTTON_ID                 = 4;
+      private static final int    LB_BUTTON_ID                = 5;
+      private static final int    RB_BUTTON_ID                = 6;
+      private static final int    BACK_BUTTON_ID              = 7;
+      private static final int    START_BUTTON_ID             = 8;
+      private static final int    LEFT_THUMBSTIKC_BUTTON_ID   = 9;
+      private static final int    RIGHT_THUMBSTICK_BUTTON_ID  = 10;
+      
+      // Axis Mappings 
+      private static final int    LEFT_THUMBSTICK_X_AXIS_ID   = 0;
+      private static final int    LEFT_THUMBSTICK_Y_AXIS_ID   = 1;
+      private static final int    LEFT_TRIGGER_AXIS_ID        = 2;
+      private static final int    RIGHT_TRIGGER_AXIS_ID       = 3;
+      private static final int    RIGHT_THUMBSTICK_X_AXIS_ID  = 4;
+      private static final int    RIGHT_THUMBSTICK_Y_AXIS_ID  = 5;
+      
+      
+      
+      // Instance Values
+      private final   int             port;
+      private final   Joystick        controller;
+      
+      public final    Thumbstick      leftStick;
+      public final    Thumbstick      rightStick;
+      public final    Trigger         lt;
+      public final    Trigger         rt;
+      public final    DirectionalPad  dPad;
+      public final    Button          a;
+      public final    Button          b;
+      public final    Button          x;
+      public final    Button          y;
+      public final    Button          lb;
+      public final    Button          rb;
+      public final    Button          back;
+      public final    Button          start;
+  
+*/
